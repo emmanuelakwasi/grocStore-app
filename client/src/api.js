9c6f2f275044832cae93115ae4c6539d38b8de63
Fada Mannie <opokuakwasi239@gmail.com>
2025-10-05 15:45:37 -0500

    api

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // API client
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
   withCredentials: true,
 });
 
